c1b20c004d8d9a54e375b4c15b8ce546b9045568
lllmoseslll <mosespeter620@gmail.com>
2024-03-31 19:44:04 +0300

    server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express(); 
 const port = 3000;
+
+
+mongoose.connect(process.env.MONGO).then(
+    ()=>{
+        console.log("Mongodb is connected");
+    }
+).catch(err =>{
+    console.log(err);
+});
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
