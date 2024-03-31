7e1c07bf44dbbeaecea7addbd74032b2a5ab95d1
lllmoseslll <mosespeter620@gmail.com>
2024-03-31 14:45:50 +0300

    Adding more pages and routes

---
+++ client/src/App.jsx
@@ -0,0 +1,37 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Team from "./pages/Team";
+import Nopage from "./pages/Nopage";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Dashboard from "./pages/Dashboard";
+
+
+function App() {
+  return( 
+  <>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/home" element={<Home />} />
+    <Route index element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/blog" element={<Blog />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/team" element={<Team />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-out" element={<SignOut />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="*" element={<Nopage />} />
+  </Routes>
+  </BrowserRouter>
+  </>
+  );
+}
+
+export default App;
